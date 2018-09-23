bb81f460bffbcd5c170b99e6d4513604ff0fd50b
Stefan Hepp <stefan@stefant.org>
2018-09-24 01:19:03 +0200

    Added timer for long presses, bugfixes. First working version.

--- homecontrol/src/main.c
+++ homecontrol/src/main.c
@@ -7,18 +7,18 @@
  *
  * Pinout:
  * - PortB:
- *   - PB0: DIn0
- *   - PB1: DIn1
- *   - PB2: DIn2
- *   - PB3: DIn3
+ *   - PB0: DIn0 - Livingroom
+ *   - PB1: DIn1 - Staircase
+ *   - PB2: DIn2 - Stair-Light
+ *   - PB3: DIn3 - Stair-Staircase
  *   - PB4: DIn4
  *   - PB5: DIn5
  *   - PB6: DIn6
  *   - PB7: DIn7
  * - PortC:
- *   - PC0: DOut0
- *   - PC1: DOut1
- *   - PC2: Sense In 0
+ *   - PC0: DOut0 - Livingroom
+ *   - PC1: DOut1 - Staircase
+ *   - PC2: Sense In 0 - Stair Sense
  *   - PC3: Sense In 1
  *   - PC4: SDA
  *   - PC5: SCL
@@ -26,24 +26,25 @@
  * - PortD:
  *   - PD0: RXD
  *   - PD1: TXD
- *   - PD2: DOut2
- *   - PD3: DOut3
+ *   - PD2: DOut2 - Stair-Light
+ *   - PD3: DOut3 - Stair-Sense
  *   - PD4: DOut4
  *   - PD5: DOut5
  *   - PD6: DOut6
  *   - PD7: DOut7
  **/
 
-#include <avr/io.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
-
 // Define CPU clock speed and UART baud rate
 #define F_CPU 1000000
 #define BAUD  9600
 
-// Baud rate calculation
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+// Baud rate calculation, delays
 #include <util/setbaud.h>
+#include <util/delay.h>
 
 #define SET_BIT(port,pin,value) \
     if (value) { \
@@ -72,9 +73,10 @@
 // - Sense notification. Value = 1: Motion detected
 #define UART_CMD_SENSE         0x06
 
-#define P_OFF   0
-#define P_SHORT 1
-#define P_LONG  2
+#define P_OFF     0
+#define P_SHORT   1
+#define P_LONG    2
+#define P_PRESSED 3
 
 typedef struct {
     unsigned char  state;
@@ -111,14 +113,85 @@ uart_buf_t UART_RX_Buf;
 
 void sleep(void)
 {
+    cli();
+
     // Only sleep it there is no input change since last read
     if (Last_input == PINB) {
 	sleep_enable();
-	//sleep_bod_disable();
+	sleep_bod_disable();
 	sei();
 	sleep_cpu();
 	sleep_disable();
+    } else {
+	sei();
     }
+}
+
+/**
+ * Start the long-press timer for that switch
+ */
+void start_timer(switch_t *sw)
+{
+    sw->time_longpress = 20;
+    // Enable timer
+    TCCR1B = 0x1D;
+}
+
+void init_timer(void)
+{
+    // Fast PWM mode, OCR1A top (mode 15), prescaler clk/1024
+    TCCR1A = 0x03;
+    TCCR1B = 0x18;
+
+    // Set timer to 100ms
+    OCR1A = ((F_CPU/1024)/10);
+
+    // Enable interrupts
+    TIMSK1 = _BV(OCIE1A);
+}
+
+/**
+ * Update switch status on 100ms timer events.
+ *
+ * \return 1 if switch requires another timer event, else 0.
+ */
+char update_timer(switch_t *sw)
+{
+    if (sw->pressed != P_PRESSED) {
+	// Disable timer if switch is released
+	sw->time_longpress = 0;
+	return 0;
+    }
+    if (sw->time_longpress) {
+	sw->time_longpress--;
+	if (sw->time_longpress > 0) {
+	    return 1;
+	} else {
+	    sw->pressed = P_LONG;
+	    return 0;
+	}
+    }
+    return 0;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+    unsigned char cnt = 0;
+
+    cnt += update_timer(&SW_Livingroom);
+    cnt += update_timer(&SW_Staircase);
+    cnt += update_timer(&SW_Stair_Light);
+    cnt += update_timer(&SW_Stair_Staircase);
+
+    if (cnt == 0) {
+	// Stop timer
+	TCCR1B = 0x18;
+    }
+}
+
+ISR(PCINT0_vect)
+{
+    // Register interrupt handler to wakeup CPU
 }
 
 void init_switch(switch_t *sw)
@@ -128,11 +201,26 @@ void init_switch(switch_t *sw)
     sw->time_longpress = 0;
 }
 
+/**
+ * Update the status of a switch from an pin input.
+ */
 void update_switch(switch_t *sw, unsigned char value)
 {
-    
+    if (!sw->state && value) {
+	start_timer(sw);
+	sw->pressed = P_PRESSED;
+    }
+    if (sw->state && !value) {
+	if (sw->pressed == P_PRESSED) {
+	    sw->pressed = P_SHORT;
+	}
+    }
+    sw->state = value;
 }
 
+/**
+ * Add a value to a FIFO buffer.
+ */
 void buf_push(uart_buf_t *buf, unsigned char value)
 {
     if (buf->len < UART_BUF_SZ) {
@@ -141,26 +229,37 @@ void buf_push(uart_buf_t *buf, unsigned char value)
     }
 }
 
+/**
+ * Remove a value from a FIFO buffer.
+ * \return the buffer, or 0 if the buffer is empty
+ */
 unsigned char buf_pop(uart_buf_t *buf)
 {
     unsigned char ret = 0;
 
     if (buf->len > 0) {
 	ret = buf->buf[buf->start];
-	buf->start++;
+	buf->start = (buf->start + 1) % UART_BUF_SZ;
 	buf->len--;
     }
     return ret;
 }
 
+/**
+ * Handle receiving a character from UART.
+ */
 ISR(USART_RX_vect)
 {
     buf_push( &UART_RX_Buf, UDR0 );
 }
 
+/**
+ * Send the next character when the send buffer is empty.
+ */
 ISR(USART_UDRE_vect)
 {
     if (UART_TX_Buf.len > 0) {
+	// Send the next character from the buffer
 	UDR0 = buf_pop( &UART_TX_Buf );
     } else {
 	// Stop transmitting
@@ -168,30 +267,64 @@ ISR(USART_UDRE_vect)
     }
 }
 
+/**
+ * Init UART.
+ **/
+void init_uart(void)
+{
+    // Set baud rate
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~_BV(U2X0);
+#endif
+    // Enable RX and TX, RX interrupts
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);
+    // Set frame format: 8N1
+    UCSR0C = (3 << UCSZ00);
+}
+
+/**
+ * Send a command via UART
+ */
 void send_command(unsigned char cmd, unsigned char value)
 {
+    cli();
     buf_push( &UART_TX_Buf, UART_CMD_HEADER | cmd );
     buf_push( &UART_TX_Buf, value );
+    sei();
 
-    // Start transmitting
+    // Start transmitting (enable UDRE interrupt on empty buffer)
     UCSR0B |= _BV( UDRIE0 );
 }
 
+/**
+ * Read a command received from UART.
+ * \return 1 if a command is received, else 0.
+ */
 char read_command(unsigned char *cmd, unsigned char *value)
 {
     // We need at least 2 characters in the buffer for a valid message
+    cli();
     while (UART_RX_Buf.len > 1) {
 	*cmd = buf_pop(&UART_RX_Buf);
 	if ((*cmd & UART_CMD_HEADER) == UART_CMD_HEADER) {
 	    if (UART_RX_Buf.len > 0) {
+		*cmd &= ~(UART_CMD_HEADER);
 		*value = buf_pop(&UART_RX_Buf);
+		sei();
+		return 1;
 	    } else {
 		// Not yet enough data
 		buf_push(&UART_RX_Buf, *cmd);
+		sei();
 		return 0;
 	    }
 	}
     }
+    sei();
     return 0;
 }
 
@@ -228,12 +361,12 @@ void read_inputs(void)
 {
     Last_input = PINB;
 
-    update_switch(&SW_Livingroom,      Last_input & (1 << PB0));
-    update_switch(&SW_Staircase,       Last_input & (1 << PB1));
-    update_switch(&SW_Stair_Light,     Last_input & (1 << PB2));
-    update_switch(&SW_Stair_Staircase, Last_input & (1 << PB3));
+    update_switch(&SW_Livingroom,      Last_input & (1 << PB0) ? 1 : 0);
+    update_switch(&SW_Staircase,       Last_input & (1 << PB1) ? 1 : 0);
+    update_switch(&SW_Stair_Light,     Last_input & (1 << PB2) ? 1 : 0);
+    update_switch(&SW_Stair_Staircase, Last_input & (1 << PB3) ? 1 : 0);
 
-    update_switch(&SW_Stair_Sense,     bit_is_set(PINC, PC2));
+    update_switch(&SW_Stair_Sense,     bit_is_set(PINC, PC2) ? 1 : 0);
 }
 
 /**
@@ -281,6 +414,7 @@ void process_inputs(void)
 	send_command(UART_CMD_LIVINGROOM, DO_Livingroom);
     }
     if (SW_Livingroom.pressed == P_LONG) {
+	// Send UART command
 	send_command(UART_CMD_LIVINGROOM, 2);
     }
 
@@ -299,7 +433,7 @@ void process_inputs(void)
 
     // Stair light switch
     if (SW_Stair_Light.pressed == P_SHORT) {
-	// Toggle sensor on/off
+	// Toggle On > Sensor <> Off
 	if (DO_Stair_Light) {
 	    DO_Stair_Light = 0;
 	    DO_Stair_Sense = 1;
@@ -309,7 +443,7 @@ void process_inputs(void)
 	send_command(UART_CMD_STAIR, (DO_Stair_Sense << 1) | DO_Stair_Light);
     }
     if (SW_Stair_Light.pressed == P_LONG) {
-	// Toggle light on/off
+	// Toggle light on/off, sensor off
 	DO_Stair_Light ^= 1;
         DO_Stair_Sense = 0;
 	send_command(UART_CMD_STAIR, (DO_Stair_Sense << 1) | DO_Stair_Light);
@@ -317,10 +451,12 @@ void process_inputs(void)
 
     // Outside stair staircase switch
     if (SW_Stair_Staircase.pressed == P_SHORT) {
+	// Toggle staircase
 	DO_Staircase ^= 1;
 	send_command(UART_CMD_STAIRCASE, DO_Staircase);
     }
     if (SW_Stair_Staircase.pressed == P_LONG) {
+	// Send UART command
 	send_command(UART_CMD_STAIRCASE, 2);
     }
 
@@ -330,10 +466,18 @@ void process_inputs(void)
     }
 
     // All new inputs processed, clear status
-    SW_Livingroom.pressed = P_OFF;
-    SW_Staircase.pressed = P_OFF;
-    SW_Stair_Light.pressed = P_OFF;
-    SW_Stair_Staircase.pressed = P_OFF;
+    if (SW_Livingroom.pressed != P_PRESSED) {
+	SW_Livingroom.pressed = P_OFF;
+    }
+    if (SW_Staircase.pressed != P_PRESSED) {
+	SW_Staircase.pressed = P_OFF;
+    }
+    if (SW_Stair_Light.pressed != P_PRESSED) {
+	SW_Stair_Light.pressed = P_OFF;
+    }
+    if (SW_Stair_Staircase.pressed != P_PRESSED) {
+        SW_Stair_Staircase.pressed = P_OFF;
+    }
     SW_Stair_Sense.pressed = P_OFF;
 }
 
@@ -345,62 +489,53 @@ void write_outputs(void)
     SET_BIT(PORTD, PD3, DO_Stair_Sense);
 }
 
-int main()
-{    
-    // *** 
-    // Disable interrupts
-    // ***
-    cli();
-
-    // ***
-    // Setup ports 
-    // ***
+/**
+ * Init IO ports.
+ */
+void init_io(void)
+{
     // Port B: Input, external pull-down
     DDRB = 0x00;
     PORTB = 0x00;
 
-    // Port C: 0,1: Output; 2-6: Input, pull-up
-    DDRC = 0xFC;
-    PORTC = 0xFC;
+    // Port C: 0,1: Output; 2-3: Input, pull-down; 4-6: Input pull-up
+    DDRC = 0x03;
+    PORTC = 0xF0;
     
-    // Port D: 0: Input; 1-7: Output
-    DDRD = 0x01;
+    // Port D: 0: Input, pull-up; 1-7: Output
+    DDRD = 0xFE;
     PORTD = 0x01;
 
-    // ***
-    // Configure UART
-    // ***
-    // Set baud rate
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
-#if USE_2X
-    UCSR0A |= _BV(U2X0);
-#else
-    UCSR0A &= ~_BV(U2X0);
-#endif
-    // Enable RX and TX, RX interrupts
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);
-    // Set frame format: 8N1
-    UCSR0C = (3 << UCSZ00);
+    // Enable external interrupts on inputs
+    PCMSK0 = 0xFF;
+    PCICR = _BV(PCIE0);
+}
 
-    // ***
+int main()
+{    
+    // Disable interrupts
+    cli();
+
+    // Setup ports 
+    init_io();
+
+    // Configure UART
+    init_uart();
+
     // Configure timer
-    // ***
+    init_timer();
 
     init_switch(&SW_Livingroom);
     init_switch(&SW_Staircase);
     init_switch(&SW_Stair_Light);
     init_switch(&SW_Stair_Staircase);
 
-
-    // ***
     // Enable interrupts
-    // ***
     sei();
 
     while(1) {
 	// Wait for any inputs
-	sleep();
+	//sleep();
 
 	read_inputs();
 
@@ -409,6 +544,9 @@ int main()
 	process_uart();
 
 	write_outputs();
+
+	// Debounce inputs
+	_delay_ms(25);
     }
 
     return 0;
