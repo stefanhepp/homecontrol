5417affe5416dd6a7de3acb2b2eab00545df8c17
Stefan Hepp <stefan@stefant.org>
2018-10-10 21:08:58 +0200

    Added GPLv3 header

--- pixtend/pixcontrol.py
+++ pixtend/pixcontrol.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
+#
+# Project:   HomeControl
+# File:      pixcontrol.py
+# Author:    Stefan Hepp <stefan@stefant.org>
+#
+# Copyright 2018 Stefan Hepp
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Control loop implementation for my home controller.
+#
+# This software is intended to be run on a PiXtend v2-S board.
+#
 
 # Import Pixtend V2 class
 from pixtendv2s import PiXtendV2S
