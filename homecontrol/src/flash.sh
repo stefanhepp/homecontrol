c9e3f2a270c93580327d0f17d998082019378d02
Stefan Hepp <stefan@stefant.org>
2023-05-14 13:33:35 +0200

    Update flash script for Pololu ISP

--- homecontrol/src/flash.sh
+++ homecontrol/src/flash.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #
-# Flash script for HomeControl AtMega48 using avrdude on RaspberryPi
+# Flash script for HomeControl AtMega48 using avrdude
 #
-sudo avrdude -c linuxgpio -p atmega48 -v -U flash:w:control.hex:i
+
+# Flash using avrdude on Raspberry PI
+#sudo avrdude -c linuxgpio -p atmega48 -v -U flash:w:control.hex:i
+
+# Flash using Pololu ISP USB programmer (use pavr2gui / pavr2cmd to change VOut settings)
+sudo avrdude -c avrisp2 -P /dev/ttyACM0 -p atmega48 -v -U flash:w:control.hex:i
+
